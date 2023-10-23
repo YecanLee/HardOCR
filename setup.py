5d0c7f40a1cc05e795652fc58a96c1ca06e4d465
Li <M.Li@campus.lmu.de>
2023-10-23 22:58:23 +0200

    Update the whole training scirpt for HardOCR

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple installation of ocr detection network with cnn+lstm',
+    author='Meimingwei Li',
+    license='MIT',
+)
