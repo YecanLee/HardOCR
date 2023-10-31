05c51fe3f904beb78bc44d65597469a80406de8c
Li <M.Li@campus.lmu.de>
2023-10-31 22:46:53 +0100

    lightning version update

--- notebooks/torchlight_version.py
+++ notebooks/torchlight_version.py
@@ -9,8 +9,15 @@ from torchvision import models,transforms
 from torchvision.transforms import ToTensor
 import lightning.pytorch as pl
 from models.CNNLSTM import ModifiedResNet 
-import albumentations as A
+# import albumentations as A
+from transformers import PreTrainedTokenizerFast
 
+# Define the tokenizer
+tokenizer = PreTrainedTokenizerFast(tokenizer_file = 'C:/Users/ra78lof/occinference/byte-level-BPE.tokenizer.json')
+
+# Add PAD token to the vocabulary, otherwise it will throw an error
+tokenizer.add_special_tokens({'pad_token': "pad_token"})
+special_tokenizer = tokenizer 
 
 # Define the lightning class, this includes a transfer learning model and a loss function
 class OcrModel(pl.LightningModule):
@@ -21,6 +28,7 @@ class OcrModel(pl.LightningModule):
         encoder_decoder = ModifiedResNet(backbone)
         self.encoder_decoder = encoder_decoder 
         self.loss = nn.CrossEntropyLoss(dim = 2) # CrossEntropyloss on the third dimension  
+        self.save_hyperparameters() # Save the hyperparameters for logging purposes
 
     def training_step(self, batch, batch_idx):  
         x, y = batch
@@ -35,7 +43,7 @@ class OcrModel(pl.LightningModule):
         return loss
     
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=0.01, weight_decay=0.0001)  
+        optimizer = optim.SGD(self.parameters(), lr=0.01, weight_decay=0.0001)  
         scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.001, max_lr=0.01, step_size_up=100, cycle_momentum=False)
         return [optimizer], [scheduler]
     
@@ -43,53 +51,49 @@ class OcrDataModule(pl.LightningDataModule):
     def __init__(self, 
                  train_batch_size: int, 
                  val_batch_size: int, 
-                 test_batch_size: int,
-                 tokenizer = None, 
-                 feature_extractor = None, 
-                 transforms=None, 
-                 max_target_length = 45):
+                 test_batch_size: int):
         super().__init__()
         self.train_batch_size = train_batch_size
         self.val_batch_size = val_batch_size
         self.test_batch_size = test_batch_size
+        self.tokenizer = special_tokenizer
         self.transforms = transforms.Compose([transforms.Resize((500, 1200)), 
                                               transforms.ToTensor(),
                                               transforms.Normalize((0.5,), (0.5,))]) # Normalize the images to the range [-1, 1], this is a grayscale image.
 
     def prepare_data(self):
         # Load the Data from the excel files, load the images
-        train_set = pd.read_excel(os.getcwd() + '/data/dom_project/Train_data.xlsx')
-        valid_set = pd.read_excel(os.getcwd() + '/data/dom_project/Val_data.xlsx')
-        test_set = pd.read_excel(os.getcwd() + '/data/dom_project/Test_data.xlsx')  
-        img_names = train_set['Image_name'].tolist()
+        self.train_set = CustomDataset(excel_file=os.getcwd() + '/Train_data.xlsx',
+                             img_dir=os.getcwd() + '/Train_data/', tokenizer = tokenizer, transform=transforms)
+        self.valid_set = CustomDataset(excel_file=os.getcwd() + '/Valid_data.xlsx',
+                             img_dir=os.getcwd() + '/Valid_data/', tokenizer = tokenizer, transform=transforms)
+        self.test_set = CustomDataset(excel_file=os.getcwd() + '/Test_data.xlsx',
+                             img_dir=os.getcwd() + '/Test_data/', tokenizer = tokenizer, transform=transforms)
+        
 
-    def set_up(self):
+    def set_up(self, stage: str):
+        if stage == 'fit':
+            self.train_dataset = self.train_set
+        if stage == 'validate':
+            self.valid_dataset = self.valid_set
+        if stage == 'test':
+            self.test_dataset = self.test_set
         # Define the train, validation and test datasets we got from prepare_data(), use the transforms we defined above
         pass
 
     def train_dataloader(self):
-        train_dataset = DataLoader(train_dataset, batch_size=self.train_batch_size, shuffle=True)
+        return DataLoader(self.train_set, batch_size=self.train_batch_size, shuffle=True)
 
     def val_dataloader(self):
-        valid_dataset = DataLoader(valid_dataset, batch_size=self.val_batch_size, shuffle=True)
+        return DataLoader(self.valid_set, batch_size=self.val_batch_size, shuffle=True)
 
     def test_dataloader(self): 
-        test_dataset = DataLoader(test_dataset, batch_size=self.test_batch_size, shuffle=True)
+        return DataLoader(self.test_set, batch_size=self.test_batch_size, shuffle=True)
 
-
-
-
-# Define the dataloader
-train_dataset = CustomDataset(excel_file='C:/Users/ra78lof/occinference/Test_data.xlsx',
-                             img_dir='C:/Users/ra78lof/occinference/Test_data/', tokenizer = tokenizer, transform=transforms)
-
-valid_dataset = CustomDataset(excel_file='C:/Users/LMMISTA-WAP265/OcciGen/data/dom_project/Val_data.xlsx',
-                              img_dir='C:/Users/LMMISTA-WAP265/OcciGen/data/dom_project/Val_data/', tokenizer = tokenizer, transform=transforms)
-
-test_dataset = CustomDataset(excel_file='C:/Users/ra78lof/occinference/Test_data.xlsx',
-                             img_dir='C:/Users/ra78lof/occinference/Test_data/', tokenizer = tokenizer, transform=transforms)
     
-
+dm = OcrDataModule(32, 64, 64)
 model = OcrModel()
 trainer = pl.Trainer(gpus = 1, max_epochs = 200)
-trainer.fit(model, train_dataset, valid_dataset)
+trainer.fit(model, datamodule = dm)
+trainer.test(datamodule=dm)
+trainer.validate(datamodule=dm)
